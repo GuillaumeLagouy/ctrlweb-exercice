1bfca929e28b95b2d9add7ad6c35633c09acc50d
GuillaumeLagouy1 <glagouy.pro@gmail.com>
2024-02-09 16:37:39 -0500

    Add all img and icons assets

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<h1 className="text-4xl font-bold underline">
-      Hello world!
-</h1>
+    <div>
+        <h1 className="text-4xl font-bold underline">Hello world!</h1>
+        <img src="@/assets/img/newsletter_illustration.svg" alt="" srcset="" />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
